49b1271a81b4b0fd42d257f029e9587307f293db
Hiren Chhatbar <hc.rajkot@gmail.com>
2023-12-29 14:37:03 +0530

    feat(system): initial setup

---
+++ init.lua
@@ -0,0 +1,26 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- init https://github.com/folke/lazy.nvim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    require "plugins.which-key",
+    require "plugins.telescope",
+}, opts)
+
+require "keymaps.telescope"
