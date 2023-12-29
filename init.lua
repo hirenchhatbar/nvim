05e04c2368f4207ab2446119fda38a81cfe4d5cf
Hiren Chhatbar <hc.rajkot@gmail.com>
2023-12-29 18:10:58 +0530

    feat(system): update

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+local function script_path()
+  local str = debug.getinfo(2, 'S').source:sub(2)
+  return str:match('(.*' .. '/' .. ')')
+end
+
+package.path = package.path .. ';' .. script_path() ..'/?.lua'
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
